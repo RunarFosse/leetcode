143c8e0c2aa250abbc86acf73adf2d96e4123e4e
Runar Fosse <rfosse01@gmail.com>
2023-10-13 22:45:38 +0200

    Updated update-readme.sh

--- update-readme.sh
+++ update-readme.sh
@@ -3,4 +3,5 @@
 name=$1
 time=$(date)
 
+echo -e "# My Leetcode Solutions\nAll problems are tried solving in the most optimal way possible." > README.md
 echo -e "\n\nLatest push from $name: $time." >> README.md
