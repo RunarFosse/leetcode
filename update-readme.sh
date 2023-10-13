3f9cca9101e3d237cf3d003605ee96921e16c7e7
Runar Fosse <rfosse01@gmail.com>
2023-10-13 23:26:48 +0200

    Added problem display to update-readme.sh

--- update-readme.sh
+++ update-readme.sh
@@ -3,5 +3,36 @@
 name=$1
 time=$(date)
 
+
+print_directory_problems () {
+    local directory=$1
+    cd $directory
+
+    # Print directory title
+    echo "\n## $directory\n"
+
+    # Print table headers
+    echo "| Problem | Time complexity | Space complexity | Leetcode |\n"
+    echo "| :------ | :-------------: | :--------------: | :--------------: |\n"
+
+    for f in *.py; do
+        # Extract Big O notation from time- and space-complexity
+        local time_complexity=$(cat $f | grep "Time complexity:" -m 1 | grep -oE "O\(.*\)")
+        local space_complexity=$(cat $f | grep "Space complexity:" -m 1 | grep -oE "O\(.*\)")
+
+        # Print table row
+        echo "| [$f]($directory/$f) | $time_complexity | $space_complexity | [Link](https://leetcode.com/problems/$f/)\n"
+    done
+    cd ..
+}
+
+# Print main title and description
 echo -e "# My Leetcode Solutions\nAll problems are tried solving in the most optimal way possible." > README.md
+
+# Print all directories
+echo -e $(print_directory_problems Easy) >> README.md
+echo -e $(print_directory_problems Medium) >> README.md
+echo -e $(print_directory_problems Hard) >> README.md
+
+# Print last updated and by who
 echo -e "\n\nLatest push from $name: $time." >> README.md
