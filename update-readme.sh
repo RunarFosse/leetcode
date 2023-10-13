b182367010cdb0645a4c0b90f8f5bb4865a38d8d
Runar Fosse <rfosse01@gmail.com>
2023-10-13 23:29:41 +0200

    Removed file ending from leetcode link

--- update-readme.sh
+++ update-readme.sh
@@ -21,7 +21,7 @@ print_directory_problems () {
         local space_complexity=$(cat $f | grep "Space complexity:" -m 1 | grep -oE "O\(.*\)")
 
         # Print table row
-        echo "| [$f]($directory/$f) | $time_complexity | $space_complexity | [Link](https://leetcode.com/problems/$f/)\n"
+        echo "| [$f]($directory/$f) | $time_complexity | $space_complexity | [Link](https://leetcode.com/problems/${f%.py}/)\n"
     done
     cd ..
 }
