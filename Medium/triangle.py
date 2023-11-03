b945f0cbbb0a2b0c8138a4e9fcc68430ab83ae85
Runar Fosse <rfosse01@gmail.com>
2023-11-03 10:02:44 +0100

    Solved Triangle

---
+++ Medium/triangle.py
@@ -0,0 +1,27 @@
+# Author: Runar Fosse
+# Time complexity: O(n)
+# Space complexity: O(n)
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Using dynamic programming
+        height = len(triangle)
+        opt = triangle
+
+        for i in reversed(range(height-1)):
+            for j in range(len(triangle[i])):
+                opt[i][j] = min(opt[i+1][j], opt[i+1][j+1]) + triangle[i][j]
+        
+        return opt[0][0]
+
+    
+# For each "node" we have to traverse down every path, storing
+# minimum path sum for each node
+
+# opt(i, j) - minimum path sum for row i, index j 
+
+# Base case:
+# opt(tree height, j) = triangle[tree height][j]
+
+# Recurrency:
+# opt(i, j) = min(opt(i+1, j), opt(i+1, j+1)) + triangle[i][j]
