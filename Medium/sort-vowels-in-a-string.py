4b132d153bb06a15366aeea0ec055e2cf97678d7
Runar Fosse <rfosse01@gmail.com>
2023-11-13 10:09:09 +0100

    Solved Sort Vowels in a String

--- Medium/sort-vowels-in-a-string.py
+++ Medium/sort-vowels-in-a-string.py
@@ -1,27 +1,29 @@
 # Author: Runar Fosse
-# Time complexity: O(nlog n)
-# Space complexity: O(n)
+# Time complexity: O(n)
+# Space complexity: O(1)
 
 class Solution:
     def sortVowels(self, s: str) -> str:
-        # First extract vowel string
-        vowels = { "a", "e", "i", "o", "u" }
-        vowelstring = []
-        for c in s:
-            if c.lower() in vowels:
-                vowelstring.append(c)
-        
-        # Sort vowelstring
-        vowelstring.sort()
+        # Using Counting sort
+        vowel_string = "AEIOUaeiou"
 
-        # Recreate string with sorted vowels
-        i = 0
+        # First count occurence of each vowel
+        vowels = set(vowel_string)
+        counts = defaultdict(lambda : 0)
+        for c in s:
+            if c in vowels:
+                counts[c] += 1
+        
+        # Then recreate string, substituting vowels for sorted order (order of the set vowels)
+        vowels_sorted = deque(vowel_string)
         string = ""
         for c in s:
-            if c.lower() in vowels:
-                string += vowelstring[i]
-                i += 1
+            if c in vowels:
+                while not counts[vowels_sorted[0]]:
+                    vowels_sorted.popleft()
+                string += vowels_sorted[0]
+                counts[vowels_sorted[0]] -= 1
             else:
                 string += c
-        
+
         return string
