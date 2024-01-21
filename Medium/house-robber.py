a8ccd1522d4f1c823652f86b923cb58fa17d5a5d
Runar Fosse <rfosse01@gmail.com>
2024-01-21 12:04:51 +0100

    Solved House Robber

--- Medium/house-robber.py
+++ Medium/house-robber.py
@@ -1,23 +1,26 @@
 # Author: Runar Fosse
 # Time complexity: O(n)
-# Space complexity: O(n)
+# Space complexity: O(1)
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
         # Using dynamic programming
         n = len(nums)
         
-        opt = [0] * n
-        for i in range(n):
+        for i in range(1, n):
             # Base cases
-            if i in [0, 1]:
-                opt[i] = max(nums[1], nums[0]) if i else nums[0]
+            if i == 1:
+                if nums[0] > nums[1]:
+                    nums[1] = nums[0]
                 continue
 
             # Recurrency
-            opt[i] = max(nums[i] + opt[i-2], opt[i-1])
+            if nums[i-1] < nums[i] + nums[i-2]:
+                nums[i] += nums[i-2]
+            else:
+                nums[i] = nums[i-1]
         
-        return opt[n-1]
+        return nums[n-1]
 
 # opt(i) - Maximum amount of money you can rob from houses [0, i]
 #          without alerting the police.
@@ -30,3 +33,4 @@ class Solution:
 # opt(i) = max(nums[i] + opt(i-2), opt(i-1))
 
 # n.o. states = n, runtime per state O(1).
+# Implementing in-place dp gives space complexity of O(1).
