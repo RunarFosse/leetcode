9a086d73323dd964c9dec069429ece399471eec4
Runar Fosse <rfosse01@gmail.com>
2024-01-23 10:03:58 +0100

    Solved Maximum Length of a Concatenated String with Unique Characters

---
+++ Medium/maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -0,0 +1,42 @@
+# Author: Runar Fosse
+# Time complexity: O(nm)
+# Space complexity: O(n)
+
+class Solution:
+    def maxLength(self, arr: List[str]) -> int:
+        # First turn into numbers
+        self.strings = []
+        for string in arr:
+            bits = 0
+            valid = True
+            for c in string:
+                # Verify string contains no duplicates
+                if bits >> ord(c) - ord("a") & 1:
+                    valid = False
+                    break
+                # If not, set the current bit
+                bits |= pow(2, ord(c) - ord("a"))
+            
+            if valid:
+                self.strings.append(bits)
+        
+        # Then perform DFS, finding the longest string with unique characters
+        self.n = len(self.strings)
+        return self.dfs(0, 0)
+
+    def dfs(self, index: int, bits: int) -> int:
+        # Start with max_length initialized at current string length
+        max_length = bits.bit_count()
+        
+        # Then check if any other possible concatenations leads to longer string
+        for i in range(index, self.n):
+            if bits & self.strings[i]:
+                continue
+            max_length = max(self.dfs(i+1, bits | self.strings[i]), max_length)
+            
+        # Return the maximum length
+        return max_length
+
+# First we turn each string in arr into number form, where bit i is set to 1 if
+# the string contains letter i. Whilst doing this we also prune strings in arr
+# containing duplicate characters.
