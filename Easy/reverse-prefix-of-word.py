a6dcc9caca87b6198990fde38bafd4abc6342000
Runar Fosse <rfosse01@gmail.com>
2024-05-01 10:16:52 +0200

    Solved Reverse Prefix of Word

--- Easy/reverse-prefix-of-word.py
+++ Easy/reverse-prefix-of-word.py
@@ -9,11 +9,7 @@ class Solution:
         # Find first occurence of ch
         for i in range(n):
             if word[i] == ch:
-                # If at the end, reverse whole string
-                if i == n-1:
-                    return word[::-1]
-                
-                # If not, construct string with prefix reversed
+                # Construct string with prefix reversed
                 return word[(i-n)::-1] + word[(i+1):]
         
         # If ch does not exist in word, do nothing
