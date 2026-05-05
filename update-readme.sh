66d28d72f4f4b46b53592d5c94adccd966b2ec85
Runar Fosse <rfosse01@gmail.com>
2026-05-05 20:04:46 +0200

    Redirect output directly to README.md

--- update-readme.sh
+++ update-readme.sh
@@ -9,11 +9,11 @@ print_directory_problems () {
     cd $directory
 
     # Print directory title
-    echo "\n## $directory\n"
+    echo -e "\n## $directory"
 
     # Print table headers
-    echo "| Problem | Time complexity | Space complexity | Leetcode |\n"
-    echo "| :------ | :-------------: | :--------------: | :--------------: |\n"
+    echo " | Problem | Time complexity | Space complexity | Leetcode |"
+    echo " | :------ | :-------------: | :--------------: | :--------------: |"
 
     for f in *.py; do
         # Extract Big O notation from time- and space-complexity
@@ -21,8 +21,10 @@ print_directory_problems () {
         local space_complexity=$(cat $f | grep "Space complexity:" -m 1 | grep -oE "O\(.*\)")
 
         # Print table row
-        echo "| [$f]($directory/$f) | $time_complexity | $space_complexity | <a href=\"https://leetcode.com/problems/${f%.py}/\" target=\"_blank\">Link</a>\n"
+        echo " | [$f]($directory/$f) | $time_complexity | $space_complexity | <a href=\"https://leetcode.com/problems/${f%.py}/\" target=\"_blank\">Link</a>"
     done
+    
+    echo ""
     cd ..
 }
 
@@ -30,9 +32,9 @@ print_directory_problems () {
 echo -e "# My Leetcode Solutions\nAll problems are tried solving in the most optimal way possible." > README.md
 
 # Print all directories
-echo -e $(print_directory_problems Easy) >> README.md
-echo -e $(print_directory_problems Medium) >> README.md
-echo -e $(print_directory_problems Hard) >> README.md
+print_directory_problems Easy >> README.md
+print_directory_problems Medium >> README.md
+print_directory_problems Hard >> README.md
 
 # Print last updated and by who
 echo -e "\n\nLatest push from $name: $time." >> README.md
